5369abb2598fa69bdf827a39328b654be75a9733
Gabrielle <cozartge@g.cofc.edu>
2018-11-17 22:03:53 -0800

    Created data management

--- vehicleInit.py
+++ vehicleInit.py
@@ -18,3 +18,50 @@ examples:
 
 Also we need an update function (given a vehicle ID and a vehicle instance, update the dictionary's vehicle ID with the new vehicle instance)
 """
+import json
+from vehicle import Vehicle
+
+"""Creates vehicle dictionary when data.json is empty"""
+def vehicleInit(vehicleId, vehicle):
+  storedDict = {}
+  storedDict[vehicleId] = vehicle
+
+  with open('data.json', 'w') as outfile:
+    json.dump(storedDict, outfile)
+
+"""Returns the data from data.json as a dictionary"""
+def getVehicleDataAsDict():
+  with open('data.json', 'r') as infile:
+    storedJson = json.load(infile)
+  return storedJson
+
+
+"""Pushes a new vehicile on the dictionary"""
+def updateDictionary(vechicleId, vehicle):
+  
+  # pull in json and read in dictionary
+  storedDict = getVehicleDataAsDict()
+
+  # push to dictionary 
+  storedDict[vechicleId] = vehicle
+
+  # write to json file as json
+  with open('data.json', 'w') as outfile:
+    json.dump(storedDict, outfile)
+
+def toVehicleInstance(id, json):
+
+  vehicle = Vehicle(json[id]["id"], json[id]["make"], json[id]["model"], json[id]["year"], json[id]["odometer"],eval(json[id]["location"]), json[id]["accessToken"])
+  vehicle.setTeslaAirFilterLifespan(json[id]["teslaAirFilterLifespan"])
+  vehicle.setBrakePadLifespan(json[id]["brakePadLifespan"])
+  vehicle.setBatteryLifespan(json[id]["batteryLifespan"])
+  vehicle.setWindshieldWiperLifespan(json[id]["windshieldWiperLifespan"])
+  return vehicle
+
+  # return Vehicle(json[])
+jsonTest = {"newID55555": {"id": 12455, "make": "Tesla", "model": "S3", "year": 2018, "teslaAirFilterLifespan": 27000, "brakePadLifespan": 70000, "batteryLifespan": 54000, "windshieldWiperLifespan": 13500, "odometer":[50000], "location": "[(38.665266, -121.391185)]", "accessToken": None}}
+
+print(toVehicleInstance("newID55555", jsonTest))
+# vehicleInfo = Vehicle(12455, "Tesla", "S3", 2018, 50000, (38.665266, -121.391185))
+# print(vehicleInfo.toJson())
+# updateDictionary(Vehicle(12455, "Tesla", "S3", 2018, 50000, (38.665266, -121.391185)))
