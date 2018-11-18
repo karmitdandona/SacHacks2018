0881be89292f8d00d857ec21226c751511a119c3
KARMIT\Karmit <karmit.dandona@gmail.com>
2018-11-18 02:15:41 -0800

    Backend finished, working on frontend

--- app.py
+++ app.py
@@ -13,24 +13,34 @@ def home():
 	authURL = apiHelperFunctions.clientInstance.get_auth_url(force=True)
 	return render_template('index.html', title="Home", authURL = authURL)
 
-@app.route('/callback', methods=['GET'])
+@app.route('/callback', methods=['GET', 'POST'])
 def callback():
 	code = request.args.get('code')
 	if code == "access_denied" or code == None:
 		return render_template("deniedPermissions.html", title="Denied Permissions")
+
 	access = apiHelperFunctions.clientInstance.exchange_code(code)
-	
-	return redirect(url_for("dashboard", accessToken=access["access_token"]))
 
-@app.route('/dashboard/<accessToken>', methods=["GET", "POST"])
-def dashboard(accessToken):
-	# accessToken = apiHelperFunctions.RefreshAccessToken(accessToken)
-	vehicles = apiHelperFunctions.GetVehicles(accessToken)
+	vehicles = apiHelperFunctions.GetVehicles(access["access_token"])
 	vehicleID = vehicles[0]  # always takes the first vehicle
+	tempVehicle = vehicleInit.FindVehicleInstance(vehicleID, access["access_token"])  # basically puts the vehicle for selected car manafacturer into the data.json file
 	
-	tempVehicle = vehicleInit.FindVehicleInstance(vehicleID, accessToken)
+	# return redirect(url_for("dashboard", vehicleID=vehicleID))
+	return redirect(url_for("listVehicles"))
 
-	return render_template("dashboard.html", title="dashboard")
+@app.route('/listVehicles', methods=["GET", "POST"])
+def listVehicles():
+	vehiclesDict = vehicleInit.getVehicleDataAsDict()
+	vehicleIDs = []
+	for key, val in vehiclesDict.items():
+		vehicleIDs.append(key)
+	return render_template('listVehicles.html', title="List of Vehicles", vehicleIDs=vehicleIDs)
+
+@app.route('/dashboard/<vehicleID>', methods=["GET", "POST"])
+def dashboard(vehicleID):
+# 	# accessToken = apiHelperFunctions.RefreshAccessToken(accessToken)
+	tempVehicle = vehicleInit.getVehicleDataAsDict()[vehicleID]
+	return render_template("dashboard.html", title="dashboard", data=tempVehicle)
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port=5000)
