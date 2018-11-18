9ed0e0c11adfb3f6395f105abddffa2b2dead951
KARMIT\Karmit <karmit.dandona@gmail.com>
2018-11-17 19:41:31 -0800

    Update vehicleInit

--- vehicleInit.py
+++ vehicleInit.py
@@ -6,7 +6,12 @@
 here's some helpful stuff for that second function:
 
 vehicle = smartcar.Vehicle(vehicleID, accessToken["access_token"])
-odometer = vehicle.odometer()
 location = vehicle.location()
+odometer = vehicle.odometer()
 info = vehicle.info()
+
+examples:
+{'data': {'latitude': 37.07706832885742, 'longitude': -108.27452087402344}, 'age': datetime.datetime(2018, 11, 18, 3, 16, 12, 579000, tzinfo=tzutc())}
+{'data': {'distance': 44376.44140625}, 'unit_system': 'metric', 'age': datetime.datetime(2018, 11, 18, 3, 16, 12, 339000, tzinfo=tzutc())}
+{'id': 'bf67d922-1e8a-4eb3-bffe-475feaee8e4e', 'make': 'TESLA', 'model': 'Model S', 'year': 2016}
 """
